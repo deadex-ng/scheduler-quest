5fb13818be8dc8dfae965bd2e06cf932e46ad5f9
Fumbani Banda <60394916+deadex-ng@users.noreply.github.com>
2023-01-13 22:34:37 +0000

    test

--- tests/profile.test.js
+++ tests/profile.test.js
@@ -1,36 +1,5 @@
-const puppeteer = require("puppeteer");
+#!/bin/sh
 
-const iPhone = puppeteer.devices["iPhone 11"];
+bash ./scripts/backup_daily.sh ./charts
 
-describe("Check that the profile page is rendered correctly", () => {
-  let browser, page;
-
-  beforeAll(async () => {
-    browser = await puppeteer.launch();
-    page = await browser.newPage();
-    await page.emulate(iPhone);
-  });
-
-  afterAll(async () => {
-    await page.close();
-    await browser.close();
-  });
-
-  it("Load Roger's profile page", async () => {
-    await page.goto("http://localhost:3001/@roger", {
-      waitUntil: "load",
-      timeout: 60000,
-    });
-  }, 60000);
-
-  it("Window width should not exceed mobile client's width", async () => {
-    const dimensions = await page.evaluate(() => {
-      return {
-        width: window.innerWidth,
-        clientWidth: document.documentElement.clientWidth,
-      };
-    });
-
-    await expect(dimensions.width).toBeLessThanOrEqual(dimensions.clientWidth);
-  }, 15000);
-});
+cd ./backups && ls | grep backup.tar.gz
