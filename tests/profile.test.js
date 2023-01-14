d0186914f186fc5f586b924e38e0a5fea3524849
Fumbani Banda <60394916+deadex-ng@users.noreply.github.com>
2023-01-14 13:31:27 +0000

    pwd

--- tests/profile.test.js
+++ tests/profile.test.js
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 pwd
-bash ./scripts/backup_daily.sh ./charts
+echo "we are in"
+// bash ./scripts/backup_daily.sh ./charts
 
-cd ./backups && ls | grep backup.tar.gz
+// cd ./backups && ls | grep backup.tar.gz
